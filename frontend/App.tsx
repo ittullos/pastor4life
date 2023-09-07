52cb8f827b6a2bbb70bf0343874a3f7c1cb0c0c7
Isaac Tullos <isaac.tullos@gmail.com>
2023-09-07 08:28:58 -0400

    Create NewPasswordScreen

--- frontend/App.tsx
+++ frontend/App.tsx
@@ -4,11 +4,12 @@ import SignInScreen from './src/screens/SignInScreen'
 import SignUpScreen from './src/screens/SignUpScreen/SignUpScreen'
 import ConfirmEmailScreen from './src/screens/ConfirmEmailScreen'
 import ForgotPasswordScreen from './src/screens/ForgotPasswordScreen'
+import NewPasswordScreen from './src/screens/NewPasswordScreen'
 
 export default function App() {
   return (
     <View style={styles.root}>
-      <ForgotPasswordScreen />
+      <NewPasswordScreen />
     </View>
   );
 }
