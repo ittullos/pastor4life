ea702300ecd37e92562e3c432dabfd865e1fc19a
Isaac Tullos <isaac.tullos@gmail.com>
2023-09-25 14:34:06 -0400

    Make API call for verse of the day and style

--- frontend/src/navigation/DrawerNavigator.tsx
+++ frontend/src/navigation/DrawerNavigator.tsx
@@ -1,4 +1,4 @@
-import { View, Text } from 'react-native'
+import { View, Text, Pressable } from 'react-native'
 import React from 'react'
 import { createDrawerNavigator } from '@react-navigation/drawer'
 import StatsScreen from '../screens/StatsScreen'
--- frontend/src/screens/HomeScreen/HomeScreen.tsx
+++ frontend/src/screens/HomeScreen/HomeScreen.tsx
@@ -1,30 +1,119 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text, StyleSheet,ScrollView } from 'react-native'
+import React, { useEffect, useState } from 'react'
 import { Auth } from 'aws-amplify'
+import { useFonts } from 'expo-font'
+import * as SplashScreen from 'expo-splash-screen'
+import axios from 'axios'
+
  
 const HomeScreen = () => {
+  const [verse, setVerse]            = useState('')
+  const [notation, setNotation]      = useState('')
 
-  const signOut = () => {
-    Auth.signOut()
+  // const signOut = () => {
+  //   Auth.signOut()
+  // }
+
+  useEffect(() => {
+    let ignore = false
+    if (!ignore) {
+      getVerse()
+    }
+    return () => { ignore = true }
+  },[])
+
+  const getVerse = () => {
+    axios.post(`https://pastor4life.click/p4l/home`,  { userId: "isaac.tullos@gmail.com" }
+    ).then(res => {
+      console.log("getVerse: ", res)
+      setVerse(res.data.verse)
+      setNotation(res.data.notation)
+    }).catch(err => {
+      console.log(err)
+    })
+  }
+
+
+  const [fontsLoaded] = useFonts({
+    "GeorgiaItalic": require("../../../assets/fonts/georgiai.ttf")
+  })
+
+  useEffect(() => {
+    async function prepare() {
+      await SplashScreen.preventAutoHideAsync()
+    }
+    prepare()
+  }, [])
+  
+
+  if (!fontsLoaded) {
+    return undefined
+  } else {
+    SplashScreen.hideAsync()
   }
 
   return (
-    <View style={{flex: 1}}>
-      <Text style={{fontSize: 24, alignSelf: 'center'}}>Home Sweet Home</Text>
-      <Text
-        onPress={signOut}
-        style={{
-          width: '100%',
-          textAlign: 'center',
-          color: 'red',
-          marginTop: 'auto',
-          marginVertical: 20,
-          fontSize: 20
-        }} >
-        Sign out
-      </Text>
+    <View style={styles.container}>
+      <View style={{ 
+        flex: 6, 
+        // borderWidth: 3, 
+        // borderColor: 'brown' 
+      }}>
+        <Text style={{
+          fontSize: 28, 
+          alignSelf: 'center', 
+          fontWeight: 'bold', 
+          paddingBottom: 10 
+        }}>
+          Verse of the Day
+        </Text>
+        <ScrollView>
+          <Text style={{ 
+            fontFamily: "GeorgiaItalic", 
+            paddingHorizontal: 20, 
+            fontSize: 22, 
+            textAlign: 'center', 
+            lineHeight: 30,
+            paddingTop: 8 
+          }}>
+            "{verse}"
+          </Text>
+          <Text style={{ 
+            paddingVertical: 15, 
+            fontSize: 22, 
+            textAlign: 'center', 
+            fontWeight: '600' 
+          }}>
+            - {notation}
+          </Text>
+        </ScrollView>
+      </View>
+      <View style={{ 
+        flex: 3, 
+        // borderWidth: 3, 
+        borderColor: 'brown' 
+      }}>
+
+      </View>
+      <View style={{ 
+        flex: 4, 
+        // borderWidth: 3, 
+        borderColor: 'brown' 
+      }}>
+
+      </View>
+      
     </View>
   )
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    marginTop: 90,
+    // borderWidth: 4,
+    // borderColor: 'red',
+  },
+})
 
 export default HomeScreen
